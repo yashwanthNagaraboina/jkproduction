97728ec4fb049125261405e899181b646fc8576d
NagaraboinaYashwanth1 <sandeeptrr465465@gmail.com>
2022-10-07 23:30:29 +0530

    changes

---
+++ project/src/main/java/com/example/demo/controller/hello.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+	
+	@GetMapping("/")
+	public String Hello() {
+		return "Hello my name is Modi "
+				+ " Welcome to AMAZON EC2 Online Application Made With SpringBoot";
+		
+	}
+
+}
